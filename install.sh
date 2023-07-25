79e2b8362eb123f7e7da5839262ce76f87af4eff
potatoJs <103060805+potato-Js@users.noreply.github.com>
2023-07-25 11:04:00 +0000

    made some changes

--- install.sh
+++ install.sh
@@ -36,9 +36,11 @@ function installyay() {
 
 checkup "Install yay? [Y,n]" && [ "$ans" != "n" ] && { echo "installing" && installyay; } || echo "Not installing yay"
 
+pause
+
 echo "setting up chaotic aur.."
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 echo "Append (adding to the end of the file) to /etc/pacman.conf:"
-echo "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist"
+sudo printf "%s\n%s\n" "[chaotic-aur]" "Include = /etc/pacman.d/chaotic-mirrorlist" >>/etc/pacman.conf
