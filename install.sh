d2524194e49f6587448c946e9f096475c1f48bab
punixcorn <ohyunwoods663@gmail.com>
2023-08-03 17:35:57 +0000

    rewritten script

--- install.sh
+++ install.sh
@@ -8,39 +8,31 @@ sleep 1
 
 ans=""
 
-function checkup() {
-	echo "$@"
+function run(){
+	echo "$1"
 	read ans
-}
-
-function pause() {
+	if [ "$ans" != "n" ];then
+		echo "installing" 
+		eval "$2"
+	else 
+		echo "not installing..."
+	fi 
 	sleep 1
 	ans=""
 }
 
-checkup "Install Xorg? [Y,n]" && [ "$ans" != "n" ] && { echo "installing" && sudo pacman -Sy xorg-server \
-	xorg-apps xf86-video-intel libxcb xcb-util xcb-util-keysyms \
-	xcb-util-wm xorg xorg-xinit; } || echo "not installing Xorg"
 
-pause
+run 'Install Xorg? [Y,n]' 'sudo pacman -Sy xorg-server xorg-apps xf86-video-intel libxcb xcb-util xcb-util-keysyms xcb-util-wm xorg xorg-xinit'
 
-checkup "Install Oh my ZSH? [Y,n]" && [ "$ans" != "n" ] && { echo "installing" && sudo pacman -S zsh && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; } || echo "not installing Oh My Zsh"
 
-pause
+run 'Install Oh my ZSH? [Y,n]' 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
 
-function installyay() {
-	sudo pacman -S base-devel
-	git clone https://aur.archlinux.org/yay.git
-	[ -d yay ] && cd yay && makepkg -si
-}
+run 'Install yay? [Y,n]' 'sudo pacman -S base-devel && git clone https://aur.archlinux.org/yay.git && [ -d yay ] && cd yay && makepkg -si'
 
-checkup "Install yay? [Y,n]" && [ "$ans" != "n" ] && { echo "installing" && installyay; } || echo "Not installing yay"
-
-pause
 
 echo "setting up chaotic aur.."
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-echo "Append (adding to the end of the file) to /etc/pacman.conf:"
-sudo printf "%s\n%s\n" "[chaotic-aur]" "Include = /etc/pacman.d/chaotic-mirrorlist" >>/etc/pacman.conf
+echo "Append to /etc/pacman.conf:"
+printf "%s\n%s\n" "[chaotic-aur]" "Include = /etc/pacman.d/chaotic-mirrorlist"
