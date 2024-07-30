4aa936c4c27b50fe4c63d7f282caaa73fc7062fd
punixcorn <ohyunwoods663@gmail.com>
2024-07-30 06:02:39 +0000

    fixes

--- install-zsh.sh
+++ install-zsh.sh
@@ -3,5 +3,5 @@
 # setting up zsh
 [ ! -f /bin/zsh ] && sudo pacman -S zsh
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh >zsh-setup-tmp.sh
-chmod +x ./zsh-setup-tmp
-./install-zsh.sh
+chmod +x ./zsh-setup-tmp.sh
+./zsh-setup-tmp.sh
